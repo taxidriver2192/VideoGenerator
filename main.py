1db7d56ab67725314e787c47ae582dd1171f29de
Lukas Schmidt <mr.lukas.schmid@gmail.com>
2023-09-30 22:18:37 +0200

    fix docker file, try to get port to work togetter

--- main.py
+++ main.py
@@ -5,24 +5,24 @@ import requests
 download_video_url = 'http://downloaded-videos:5001/download_video'  # Adjust as necessary
 generate_audio_url = 'http://elevenlabs-service:5002/generate_audio'  # Adjust as necessary
 
-# Define the videos directory and the video path
-videos_dir = './videos'
-video_path = os.path.join(videos_dir, 'video_1.webm')
+# Define the videos directory and check for videos files. If there are no video files, run download_video.py
+videos_dir = './files/videos'
 
-# Check if there are any video files in the './videos' directory
-video_files = [f for f in os.listdir(videos_dir) if os.path.isfile(os.path.join(videos_dir, f)) and f.endswith('.webm')]
+if not os.path.exists(videos_dir):
+    os.makedirs(videos_dir)
 
-if len(video_files) == 0:
-    # If there are no video files, download the video
-    print("No videos found in './videos' directory. Downloading a video...")
+video_files = [f for f in os.listdir(videos_dir) if os.path.isfile(os.path.join(videos_dir, f)) and f.endswith('.mp4')]
+
+if not video_files:
+    print('No video files found in the videos directory.')
+    # Run download_video.py here
+    print(f'Running download_video.py...')
     url = 'https://www.youtube.com/watch?v=UuWwIhHY9oE'
     requests.post(download_video_url, json={'url': url})
+
+    # exit program and giv information that video is being downloaded
+    print(f'Video is being downloaded')
+    exit()
+    
 else:
-    # Skip downloading as videos are already available
-    print(f"Found {len(video_files)} video(s) in './videos' directory. Skipping download.")
-
-# Generate audio and add to video
-print("Generating audio and adding it to the video...")
-requests.post(generate_audio_url, json={'video_path': video_path, 'skip_api': True})
-
-print("Process started successfully!")
+    print(f'Found {len(video_files)} video files in the videos directory.')
